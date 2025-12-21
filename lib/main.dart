cb394f73758ce355e85994d9e87ac0363617868e
Nitin Dave <nitindave2111@gmail.com>
2025-12-21 11:03:42 +0530

    introduce AuthWrapper for Firebase auth state handling

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,37 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:karma_split/pages/main_page.dart';
+import 'pages/auth_choice_page.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
   runApp(const ProviderScope(child: MyApp()));
+}
+
+class AuthWrapper extends StatelessWidget {
+  const AuthWrapper({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return StreamBuilder<User?>(
+      stream: FirebaseAuth.instance.authStateChanges(),
+      builder: (context, snapshot) {
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return const Scaffold(
+            body: Center(child: CircularProgressIndicator()),
+          );
+        }
+        if (snapshot.hasData) {
+          return const MainPage();
+        } else {
+          return const AuthChoicePage();
+        }
+      },
+    );
+  }
 }
 
 class MyApp extends StatelessWidget {
@@ -22,7 +47,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         fontFamily: 'Roboto',
       ),
-      home: MainPage(),
+      home: const AuthWrapper(),
     );
   }
 }
