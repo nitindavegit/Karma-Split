6c0b38c899123d5b06c8a1c577bed95f719cec86
Nitin Dave <nitindave2111@gmail.com>
2026-01-06 14:21:52 +0530

    added isFirstLaunch

--- lib/main.dart
+++ lib/main.dart
@@ -5,16 +5,29 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:karma_split/pages/main_page.dart';
 import 'pages/auth_choice_page.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+const String _kFirstLaunchKey = 'is_first_launch';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
   await dotenv.load(fileName: ".env");
-  runApp(const ProviderScope(child: MyApp()));
+
+  // Check if this is the first launch
+  final prefs = await SharedPreferences.getInstance();
+  final isFirstLaunch = prefs.getBool(_kFirstLaunchKey) ?? true;
+
+  if (isFirstLaunch) {
+    await prefs.setBool(_kFirstLaunchKey, false);
+  }
+
+  runApp(ProviderScope(child: MyApp(isFirstLaunch: isFirstLaunch)));
 }
 
 class AuthWrapper extends StatelessWidget {
-  const AuthWrapper({super.key});
+  final bool isFirstLaunch;
+  const AuthWrapper({super.key, this.isFirstLaunch = false});
 
   @override
   Widget build(BuildContext context) {
@@ -29,17 +42,24 @@ class AuthWrapper extends StatelessWidget {
         if (snapshot.hasData) {
           return const MainPage();
         } else {
+          // On first launch, show AuthChoicePage regardless of auth state
+          // On subsequent launches, also show AuthChoicePage when not logged in
           return const AuthChoicePage();
         }
       },
     );
   }
+
+  // Static method to check if user is logged in
+  static bool isUserLoggedIn() {
+    return FirebaseAuth.instance.currentUser != null;
+  }
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  final bool isFirstLaunch;
+  const MyApp({super.key, this.isFirstLaunch = false});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -49,7 +69,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         fontFamily: 'Roboto',
       ),
-      home: const AuthWrapper(),
+      home: AuthWrapper(isFirstLaunch: isFirstLaunch),
     );
   }
 }
